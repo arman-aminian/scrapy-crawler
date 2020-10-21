3523e0980ad6cd2f26adb726dd1f89b825a90476
Arman Aminian <arman.aminian78@gmail.com>
2020-10-21 21:32:21 +0330

    Pipeline added Mongodb added

--- TgjuCrawler/pipelines.py
+++ TgjuCrawler/pipelines.py
@@ -4,10 +4,22 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
+# useful for handling different item types with a  single interface
 from itemadapter import ItemAdapter
 
+import pymongo
 
 class TgjucrawlerPipeline:
+
+    def __init__(self):
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        db = self.conn["price_col"]
+        self.collection = db["sekee_price"]
+
+
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
